a2d181dc3fb2a0e31accbc8345d5c97774d2ca43
deepikacs <dshivalingegowda@qwinix.io>
2018-07-10 22:50:24 +0530

    first changes

--- src/Commons/Header.js
+++ src/Commons/Header.js
@@ -19,7 +19,7 @@ class Header extends Component {
             <i className="fa fa-repeat" aria-hidden="true"></i>
           </NavItem>
         <NavItem eventKey={2} href="#">
-        <i className="fa fa-play" aria-hidden="true"></i>
+        <i className="fa fa-play " aria-hidden="true"></i>
         </NavItem>
         <NavItem eventKey={2} href="#">
         <i className="fa fa-pause" aria-hidden="true">
@@ -28,28 +28,29 @@ class Header extends Component {
         <i className="fa fa-undo" aria-hidden="true">
         </i></NavItem>
           <NavItem eventKey={2} href="#">
-          <i className="fa fa-repeat " aria-hidden="true">
+          <i className="fa fa-undo gly-rotate-252" aria-hidden="true">
           </i></NavItem>
           <NavItem eventKey={2} href="#">
-          <i className="fa fa-repeat mycontent-left  " aria-hidden="true"></i>
+          <i className="fa fa-undo gly-rotate-80" aria-hidden="true"></i>
           </NavItem>
-          <NavItem eventKey={2} href="#" className="abc"></NavItem>
+          <NavItem eventKey={2} href="#" className="verticleline"></NavItem>
           <NavItem eventKey={2} href="#">
-          <i className="fa fa-search search" aria-hidden="true">
+          <i className="fa fa-search mginleft-27" aria-hidden="true">
           </i><input type="text" placeholder="shipment ID/Invoice Number"/>
            </NavItem>
-          
+          <NavItem eventKey={2} href="#" className="verticleline"></NavItem>
           <NavItem eventKey={2} href="">
-          <i className="fa fa-chevron-left mycontent-right " aria-hidden="true">
-          </i><span className="content">Last 24 hours</span></NavItem>
+          <i className="fa fa-chevron-left mginleft-27" aria-hidden="true">
+          </i><span className="margin-left20">Last 24 hours</span></NavItem>
           <NavItem eventKey={2} href="#">
-          <i className="fa fa-chevron-right mycontent-left" aria-hidden="true"></i>June 12th 10:00 hrs to June 13th 10:00 hrs
-          </NavItem>
-          <NavItem eventKey={2} href="#"><i className="fa fa-file-image-o mycontent-left" aria-hidden="true"></i></NavItem>
-        {/* </Nav>
-        <Nav pullRight> */}
+          <i className="fa fa-chevron-right marginleft-10" aria-hidden="true"></i><span className="mgnleft15">June 12th 10:00 hrs to June 13th 10:00 hrs
+          </span></NavItem>
+          <NavItem eventKey={2} href="#" className="verticleline"></NavItem>
+          <NavItem eventKey={2} href="#"><i className="fa fa-file-image-o mginleft-27 " aria-hidden="true"></i></NavItem>
+       
+       <NavItem eventKey={2} href="#" className="verticleline"></NavItem>
         <NavItem eventKey={2} href="#">
-        <i className="fa fa-share-alt" aria-hidden="true">
+        <i className="fa fa-share-alt mginleft-27" aria-hidden="true">
         </i></NavItem>
         <NavItem eventKey={2} href="#">
         <i className="fa fa-th-list" aria-hidden="true"></i>
